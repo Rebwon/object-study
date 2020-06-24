18956b9b3d9af2263db134fe6987bc95ac79fefa
Rebwon <msolo021015@naver.com>
2020-06-24 10:56:24 +0900

    객체 간 High Level Policy와 Low Level Details를 절연하지 못한 설계

---
+++ src/main/java/ko/maeng/example/object/chap1/Audience.java
@@ -0,0 +1,13 @@
+package ko.maeng.example.object.chap1;
+
+public class Audience {
+	private Bag bag;
+
+	public Audience(Bag bag) {
+		this.bag = bag;
+	}
+
+	public Bag getBag() {
+		return bag;
+	}
+}
---
+++ src/main/java/ko/maeng/example/object/chap1/Invitation.java
@@ -0,0 +1,7 @@
+package ko.maeng.example.object.chap1;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+	private LocalDateTime when;
+}
